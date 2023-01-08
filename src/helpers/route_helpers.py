90575589b78ba0fcd421e2e0940f96d9a2e6e87a
Brody <keanebrody8585@gmail.com>
2023-01-08 03:28:30 -0500

    added abstraction to todo list

--- src/helpers/route_helpers.py
+++ src/helpers/route_helpers.py
@@ -8,14 +8,27 @@ from src.forms.forms import TodoForm, AddDetailsForm
 def todo_exists(id):
     return db.session.query(Todo).get(id) is not None
 
+
 def get_todo(id):
     return Todo.query.get(id)
 
 def get_completed_todos():
     return Todo.query.filter_by(is_complete = True)
 
+def get_uncompleted_todos():
+    return Todo.query.filter_by(is_complete = False)
+
+
+def complete_todo(todo):
+    todo.is_complete = True
+
 def restore_todo(todo):
     todo.is_complete = False
+
+
+def create_todo(form):
+    return Todo(title = form.todo.data)
+
 
 def add_to_session(item):
     db.session.add(item)
--- src/routes/todo_list_routes.py
+++ src/routes/todo_list_routes.py
@@ -2,24 +2,24 @@ from flask import render_template, url_for, redirect, Blueprint
 from app import app, db
 from src.models.models import Todo
 from src.forms.forms import TodoForm
-from src.helpers.route_helpers import try_commit, todo_exists, get_todo
+from src.helpers.route_helpers import try_commit, todo_exists, get_todo, get_uncompleted_todos, create_todo, add_to_session
 
 todo_list_routes = Blueprint('todo_list_routes', __name__)
 
 @app.route('/todo-list', methods=['GET'])
 def todo_list():
-    todos = Todo.query.filter_by(is_complete = False)
-    return render_template('todo_list.html', form=TodoForm(), todos=todos, autofocus=True)
+    form = TodoForm()
+    todos = get_uncompleted_todos()
+    return render_template('todo_list.html', form=form, todos=todos, autofocus=True)
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
     form = TodoForm()
     if form.validate_on_submit():
-        new_todo = Todo(title = form.todo.data)
-        db.session.add(new_todo)
+        new_todo = create_todo(form)
+        add_to_session(new_todo)
         try_commit()
-        form.todo.data = ''
     return redirect(url_for('todo_list'))
 
 
@@ -27,7 +27,6 @@ def submit():
 def complete(id):
     if todo_exists(id):
         todo = get_todo(id)
-        todo.is_complete = True
-        db.session.add(todo)
+        add_to_session(todo)
         try_commit()
     return redirect(url_for('todo_list'))
