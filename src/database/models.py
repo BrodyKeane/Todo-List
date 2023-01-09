9c27618b10950cf1786514b30e69b5c47a847974
Brody <keanebrody8585@gmail.com>
2023-01-09 17:45:41 -0500

    data transfer

--- src/database/models.py
+++ src/database/models.py
@@ -8,9 +8,43 @@ class Todo(db.Model):
     title = db.Column(db.String(200), index = True)
     description = db.Column(db.String(1000), index = True, default = '')
     is_complete = db.Column(db.Boolean, index=True, default=False)
-    created_at = db.Column(db.DateTime(), index=True, default=datetime.utcnow)
 
     user = db.relationship('User', backref=db.backref('todo', uselist=False))
+
+    def __init__(self, title):
+        self.title = title
+        self.description = ''
+        self.is_complete = False
+    
+    def get_id(self):
+        return self.id
+
+    def get_user_id(self):
+        return self.user_id
+
+    def get_title(self):
+        return self.title
+
+    def get_description(self):
+        return self.description
+
+    def set_user_id(self, user_id):
+        self.user_id = user_id
+
+    def set_title(self, title):
+        self.title = title
+    
+    def set_description(self, description):
+        self.description = description 
+
+    def complete(self):
+        self.is_complete = True
+
+    def restore(self):
+        self.is_complete = False
+
+
+
 
 
 class User(UserMixin, db.Model):
@@ -19,6 +53,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     joined_at = db.Column(db.DateTime(), index=True, default=datetime.utcnow)
+
 
 
 class Stats(db.Model):
